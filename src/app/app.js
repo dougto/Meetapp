f3183015d2f491360d85eb3b11829f1c9563755f
Douglas Toledo <douglas22toledo@gmail.com>
2019-08-26 21:21:20 -0300

    app and test route implemented

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
-const server = express();
+const app = express();
 
-server.use(express.json());
+app.use(express.json());
+app.use(routes);
 
-server.get('', (req, res) => res.json({ message: 'blabla' }));
-
-server.listen(3000);
-
+app.listen(3000);
 // eslint-disable-next-line no-console
 console.log('Server listening on port 3000');
